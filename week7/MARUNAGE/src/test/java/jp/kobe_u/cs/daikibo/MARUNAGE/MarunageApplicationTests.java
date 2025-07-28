0fac690199e5a78055455c795f1fea0daa224f82
Tyuya-k <slsieguhatn113@gmail.com>
2025-07-28 11:35:33 +0900

    作成したプロジェクトのひな型をあげます

---
+++ week7/MARUNAGE/src/test/java/jp/kobe_u/cs/daikibo/MARUNAGE/MarunageApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.kobe_u.cs.daikibo.MARUNAGE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarunageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
